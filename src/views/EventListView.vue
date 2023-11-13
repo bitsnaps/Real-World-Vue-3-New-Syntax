4d75443546a4f222c1b290b23ea83170ff40590e
Ibrahim.H <bitsnaps@yahoo.fr>
2023-11-13 11:13:57 +0100

    implement basic navigation

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -1,18 +1,24 @@
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, computed, watchEffect } from 'vue'
 import EventCard from '@/components/EventCard.vue'
 import EventService from '@/services/EventService.js'
 
+const props = defineProps(['page'])
 const events = ref(null)
+const page = computed(() => props.page)
+const eventsPerPage = 2
 
 onMounted(() => {
-  EventService.getEvents()
+  watchEffect(() => {
+    events.value = null
+    EventService.getEvents(eventsPerPage, page.value)
     .then((response) => {
       events.value = response.data
     })
     .catch((error) => {
       console.log(error)
-    })
+    })    
+  })
 })
 </script>
 
@@ -20,6 +26,15 @@ onMounted(() => {
   <h1>Events For Good</h1>
   <div class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
+    <RouterLink
+      :to="{ name: 'event-list', query: { page: page - 1}}"
+      rel="prev"
+      v-if="page != 1"
+      >Prev Page</RouterLink>
+    <RouterLink
+      :to="{ name: 'event-list', query: { page: page + 1}}"
+      rel="next"
+      >Next Page</RouterLink>
   </div>
 </template>
 
